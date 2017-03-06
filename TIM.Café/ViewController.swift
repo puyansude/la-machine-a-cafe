b662fbc7621b425ee606097e1feb8fe9b1f50ec5
Alain <alain@macbook-pro.local>
2017-03-06 13:29:01 -0500

    Initial Commit

---
+++ TIM.Café/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TIM.Café
+//
+//  Created by Alain on 17-03-06.
+//  Copyright © 2017 Alain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
